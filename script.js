5d755d3d14d8e4ddfe474953eee2189019719ca0
Guilherme <109982337+Guilherme-Szabo@users.noreply.github.com>
2025-03-10 09:22:07 -0300

    diminua botão

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
  const aumentaFonteBotao =  document.getElementById('aumenta-fonte');
+ const diminuaFonteBotao =  document.getElementById('diminua-fonte');
+ 
  let tamanhoAtual = 1;
  aumentaFonteBotao.addEventListener('click', function() {
     tamanhoAtual += 0.1;
-    document.body.fontSize = '${tamanhoAtual}'
-    })
+    document.body.fontSize = '${tamanhoAtual}ren'
+    });
 
-}
-)
+
+
+    diminuaFonteBotao.addEventListener('click', function() {
+       tamanhoAtualI -= 0.1;
+       document.body.fontSize = '${tamanhoAtual}ren'
+       });
+   
+});
